815a426bc3a71814ed9dc07badbe6e0da4756ca3
zenitsu9028831 <126552287+zenitsu9028831@users.noreply.github.com>
2024-04-04 16:26:56 +0530

    first commit

---
+++ app/src/main/java/com/example/anonymoustraveller/SignInActivity.kt
@@ -0,0 +1,50 @@
+package com.example.anonymoustraveller
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.example.anonymoustraveller.databinding.SignInBinding
+import com.example.anonymoustraveller.databinding.SignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: SignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = SignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.loginbtn.setOnClickListener{
+            val Loginemail = binding.edemail.text.toString()
+            val Loginpassword = binding.edpassword.text.toString()
+
+
+            if(Loginemail.isNotEmpty() && Loginpassword.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(Loginemail,Loginpassword)
+                    .addOnCompleteListener {task->
+                    if (task.isSuccessful){
+
+                        startActivity(Intent(this,MainActivity::class.java))
+                       //Toast.makeText(this,"Login Sucessfully",Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this,"Login Failed",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this,"Please the Fill Info First",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        binding.txtsignup.setOnClickListener{
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/example/anonymoustraveller/SignUpActivity.kt
@@ -0,0 +1,55 @@
+package com.example.anonymoustraveller
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import com.example.anonymoustraveller.databinding.SignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: SignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = SignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.SignUpbtn.setOnClickListener {
+            val Email = binding.RegEdemail.text.toString()
+            val Password = binding.RegEdpassword1.text.toString()
+            val Confirmpsw= binding.RegEdpassword2.text.toString()
+            val Name = binding.RegEdname.text.toString()
+            val City= binding.RegEdcity.text.toString()
+
+
+
+            if(Name.isNotEmpty() && Email.isNotEmpty() && Password.isNotEmpty() && Confirmpsw.isNotEmpty()
+                && City.isNotEmpty()){
+                if(Password == Confirmpsw){
+                    firebaseAuth.createUserWithEmailAndPassword(Email,Password)
+                        .addOnCompleteListener(this){task->
+                            if(task.isSuccessful){
+                                Toast.makeText(this,"Sign Up Successfully",Toast.LENGTH_SHORT).show()
+
+                                startActivity(Intent(this,SignInActivity::class.java))
+
+                            }else{
+                                Toast.makeText(this,"something went wrong",Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                }else{
+                    Toast.makeText(this, "Password is Not Matching", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "Fill all the details", Toast.LENGTH_SHORT).show()
+                }
+
+        }
+    }
+}
